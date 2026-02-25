9038ba366d5f145696c96224ae0f343fdf59b4f5
Xisco Sastre Cabot <xiscosastre@gmail.com>
2026-02-25 20:12:18 +0100

    fix: adjust layout and formatting in Header and Section components

--- src/lib/components/Section.svelte
+++ src/lib/components/Section.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
-	import type { Snippet } from 'svelte';
+  import type { Snippet } from "svelte";
 
-	let { id, title, children }: { id: string; title?: string; children: Snippet } = $props();
+  let {
+    id,
+    title,
+    children,
+  }: { id: string; title?: string; children: Snippet } = $props();
 </script>
 
 <section
-	{id}
-	class="min-h-svh flex flex-col items-center justify-center px-6 pt-[3vh] md:pt-2 bg-white"
+  {id}
+  class="min-h-svh flex flex-col items-center justify-center px-6 pt-[3vh] md:pt-2 bg-white"
 >
-	{#if title}
-		<h2 class="text-5xl font-light font-nova text-center mb-8">{title}</h2>
-	{/if}
-	{@render children()}
+  {#if title}
+    <h2 class="text-5xl font-light font-nova text-center md:mb-8 mb-3">
+      {title}
+    </h2>
+  {/if}
+  {@render children()}
 </section>
